eb91f9fdb37c46a3623820ab30f97da9e1b9c9b3
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2012-06-25 18:10:16 -0300

    Modifications

--- markers.js
+++ markers.js
@@ -4,63 +4,55 @@ var markers = [
     lat: -34.588889,
     lng: -58.430556,
     url: 'palermo.html',
-    place: "Palermo",
-    description: "Es el personaje más entrañable de todas las historias. Andrés es linyera por elección. A los 30 años trabajaba y  estudiaba ingeniería en la UTN, pero un encuentro repentino con Dios hizo que Andrés cambie su vida radicalmente y se despojara de todo hasta quedar en la calle. Pasó por muchos barrios, pero finalmente se instaló en la Plaza Latinoamericana, y luego de siete años de vivir allí, los vecinos le regalaron un trailer para que tenga su propio techo."
+    place: "Palermo"
   },
   {
     title: "El Castillo de los Amantes Trágicos",
     lat: -34.6,
     lng: -58.483333,
     url: 'villa-del-parque.html',
-    place: "Villa del Parque",
-    description: "El comienzo de esta historia tuvo lugar el primero de abril de 1911. Lucía Giordano, única hija de un reconocido matrimonio italiano de la época, se casó con Ángel Lemos, violinista.  Hacia la madrugada, salieron de la casona donde tuvo lugar la fiesta, en Campana al 3200, en busca del auto que los trasladaría a su luna de miel, pero al cruzar las vías, un tren los arrolló. Desde entonces los ocasionales ocupantes de la casona salían despavoridos, asustados por los gritos desgarradores de una joven mujer y un violinista."
+    place: "Villa del Parque"
   },
   {
     title: "El gigante que salva vidas",
     lat: -34.6105,
     lng: -58.3976,
     url: 'once.html',
-    place: "Once",
-    description: "Según cuenta una historia de larga data, por las calles de Once vaga un personaje de casi tres metros de altura que cuida a los habitantes del barrio. Algunos afirman que es ser fue creado en el siglo XVI por un rabino de Praga, llamado Judah Loew ben Bezabel, y que llegó a Buenos Aires de la mano de un rabino, con inmigrantes judíos. Los vecinos aseguran que este gigante ha salvado a víctimas de choques y ha espantado a más de un delincuente."
+    place: "Once"
   },
   {
     title: "Venganza Arquitectónica",
     lat: -34.594722,
     lng: -58.375833,
     url: 'retiro.html',
-    place: "Retiro",
-    description: "Los Anchorena vivían en el actual Palacio San Martín con 150 sirvientes y estaban férreamente enfrentados con los Kavanagh, adinerados, aunque no patricios. Hacia 1920 los Anchorena construyen la iglesia del Santísimo Sacramento como futuro sepulcro familiar. Uno de los Anchorena se enamoró perdidamente de una Kavanagh, aunque el romance no fue aprobado por su familia. En venganza, Corina Kavanagh ordenó la construcción de un edificio en Florida y San Martín, cuyo único requisito era que impidiera la vista desde el palacio Anchorena a la iglesia, objetivo que aún cumple el edificio Kavanagh."
+    place: "Retiro"
   },
   {
     title: "El Enano Vampiro",
     lat: -34.633333,
     lng: -58.466667,
     url: 'flores.html',
-    place: "Bajo Flores",
-    description: "Belek es un enano vampiro que llegó a Buenos Aires con el Circo de los Zares a fines de los 70. El horror comienza cuando Belek se instala en una casa abandonada del Bajo Flores y los gatos del barrio comienzan a desaparecer misteriosamente. Desde entonces, los vecinos protegen sus casas con ristras de ajo y todos llevan consigo crucifijos por miedo a sus ataques."
+    place: "Bajo Flores"
   },
   {
     title: "El Hombre Sin Ojos",
     lat: -34.560694,
     lng: -58.474722,
     url: 'coghlan.html',
-    place: "Coghlan",
-    description: "Por los vagones de la línea Mitre deambulaba un hombre sin párpados que murió hace años en la estación Cohglan. El hombre fue víctima de un accidente, por fallas en la señalización de las barreras. Los vecinos creen férreamente que éste señor tiene poderes que usa para advertir a los transeúntes ante posibles accidentes."
+    place: "Coghlan"
   },
   {
     title: "El Borda se llena de luz",
     lat: -34.65,
     lng: -58.383333,
     url: 'barracas.html',
-    place: "Barracas",
-    description: "En el Hospital José T. Borda habita un ser de otro planeta llamado Solaris. Durante su estadía en el neuropsiquiátrico, Solaris, a quien describen como alguien delgado, de ojos grandes, muy blanco y completamente lampiño, se reunía con alrededor de 50 internos para recitar un mantra. Durante el rito, los internos aseguran que Solaris parece iluminarse. Además, cuentan que el grupo sanguíneo de este hombre que decía ser un Alien no encaja con ningún patrón conocido."
+    place: "Barracas"
   },
   {
     title: "El Policía Correcto",
     lat: -34.616667,
     lng: -58.45,
     url: 'caballito.html',
-    place: "Caballito",
-    description: "En la intersección de Gaona y Trelles trabaja Felipe, un policía de tránsito que es conocido en el barrio por su exigencia a la hora de trabajar. Felipe está siempre alerta, y hasta se la pasa retando a los peatones por no cruzar por la senda peatonal. Vecinos y comerciantes de la zona suelen agasajarlo con mates y facturas a la hora de la merienda, aunque ni eso lo distrae de sus deberes. "
+    place: "Caballito"
   }
 ];
