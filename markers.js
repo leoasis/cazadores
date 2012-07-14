dba63f02aa1712181b5b4b4f6ca4a8a920516dfb
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2012-07-14 20:15:29 -0300

    Updated site

--- markers.js
+++ markers.js
@@ -13,46 +13,46 @@ var markers = [
     url: 'villa-del-parque.html',
     place: "Villa del Parque"
   },
+  // {
+  //   title: "El gigante que salva vidas",
+  //   lat: -34.6105,
+  //   lng: -58.3976,
+  //   url: 'once.html',
+  //   place: "Once"
+  // },
+  // {
+  //   title: "Venganza Arquitectónica",
+  //   lat: -34.594722,
+  //   lng: -58.375833,
+  //   url: 'retiro.html',
+  //   place: "Retiro"
+  // },
   {
-    title: "El gigante que salva vidas",
-    lat: -34.6105,
-    lng: -58.3976,
-    url: 'once.html',
-    place: "Once"
-  },
-  {
-    title: "Venganza Arquitectónica",
-    lat: -34.594722,
-    lng: -58.375833,
-    url: 'retiro.html',
-    place: "Retiro"
-  },
-  {
-    title: "El Enano Vampiro",
+    title: "Belek, el enano",
     lat: -34.633333,
     lng: -58.466667,
     url: 'flores.html',
     place: "Bajo Flores"
   },
-  {
-    title: "El Hombre Sin Ojos",
-    lat: -34.560694,
-    lng: -58.474722,
-    url: 'coghlan.html',
-    place: "Coghlan"
-  },
-  {
-    title: "El Borda se llena de luz",
-    lat: -34.65,
-    lng: -58.383333,
-    url: 'barracas.html',
-    place: "Barracas"
-  },
-  {
-    title: "El Policía Correcto",
-    lat: -34.616667,
-    lng: -58.45,
-    url: 'caballito.html',
-    place: "Caballito"
-  }
+  // {
+  //   title: "El Hombre Sin Ojos",
+  //   lat: -34.560694,
+  //   lng: -58.474722,
+  //   url: 'coghlan.html',
+  //   place: "Coghlan"
+  // },
+  // {
+  //   title: "El Borda se llena de luz",
+  //   lat: -34.65,
+  //   lng: -58.383333,
+  //   url: 'barracas.html',
+  //   place: "Barracas"
+  // },
+  // {
+  //   title: "El Policía Correcto",
+  //   lat: -34.616667,
+  //   lng: -58.45,
+  //   url: 'caballito.html',
+  //   place: "Caballito"
+  // }
 ];
